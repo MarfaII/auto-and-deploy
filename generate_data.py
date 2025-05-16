27086d761fd43b6d899f513299f04743ed9dd90c
MarfaII <marfa_ii@mail.ru>
2025-05-16 12:52:40 +0500

    fix path

--- generate_data.py
+++ generate_data.py
@@ -2,9 +2,16 @@ from datetime import datetime, timedelta
 from random import randint
 import pandas as pd
 import configparser
+import os
 
+
+#путь для текущей директории
+dirname = os.path.dirname(__file__)
+
+#работаем с config
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read(os.path.join(dirname,'config.ini'))
+
 COMPANIES = eval(config['Companies']['COMPANIES'])
 today = datetime.today()
 yesterday = today - timedelta(days=1)
@@ -17,4 +24,4 @@ if 1 <= today.weekday() <= 5:
         'amount': [randint(0, 1000) for _ in range(len(COMPANIES) * 2)]
     }
 df = pd.DataFrame(d)
-df.to_csv('data_sales.csv', index=False)
+df.to_csv(os.path.join(dirname,'data_sales.csv'), index=False)
